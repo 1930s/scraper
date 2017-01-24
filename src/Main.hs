1a9bd454e3d7b596a7f3ac9d2526613d552a1875
Junjie Xie <birdeggegg@gmail.com>
2017-01-24 13:57:29 +0800

    modify parse function

--- src/Main.hs
+++ src/Main.hs
@@ -10,25 +10,17 @@ import Control.Monad
 
 import Debug.Trace
 
-type Title = String
-type Torrents = [String]
-type Archive = String
-
 searchUrl :: String -> String
 searchUrl keyword = "http://cn163.net/?s=" ++ (urlEncode keyword)
 
 openURL :: String -> IO String
 openURL x = getResponseBody =<< simpleHTTP (getRequest x)
 
-parseSearch tags = do
-  let entries = sections (~== "<div class=entry_box>") tags
-  return [(title entry, archive entry) | entry <- entries]
+parseSearch :: [Tag String] -> [(String, String)]
+parseSearch = map (\c -> (title c, archive c)) . sections (~== "<div class=entry_box>")
   where title = dropWhile isSpace . innerText . takeWhile (~/= "</h2>") . dropWhile (~/= "<div class=archive_title>")
         archive = fromAttrib "href" . head . tail . takeWhile (~/= "</a>") . dropWhile (~/= "<span class=archive_more>")
 
-
-parseTitle :: [Tag String] -> String
-parseTitle = innerText . take 2 . dropWhile (~/= "<title>")
 
 parseTorrents :: [Tag String] -> [String]
 parseTorrents = filter (isInfixOf "1024") . map f . sections (~== "<a>") . takeWhile (~/= "<div class=back_b>"). dropWhile (~/= "<div id=entry>")
@@ -37,15 +29,17 @@ parseTorrents = filter (isInfixOf "1024") . map f . sections (~== "<a>") . takeW
 getTorrents :: String -> IO ()
 getTorrents archiveUrl = do
     tags <- (parseTags . decodeString) <$> openURL archiveUrl
-    let title = parseTitle tags
-        torrents = parseTorrents tags
+    let torrents = parseTorrents tags
     putStrLn $ "torrents:\n " ++ (unlines torrents)
-  
+
+search :: String -> IO [(String, String)]
 search keyword = do
   tags <- (parseTags . decodeString) <$> openURL (searchUrl keyword)
-  entries <- parseSearch $ tags
-  return $ zip entries [0..]
+  let entries = parseSearch $ tags
+  putStrLn . unlines . map showEntry . flip zip [0..] $ entries
+  return entries
 
+showEntry :: ((String, String), Integer) -> String
 showEntry ((title, _), index) = (show index) ++ ") " ++ title
 
 data Params = Params
@@ -64,9 +58,6 @@ main = do
   case keyword of
     Params key -> do
       entries <- search key
-      putStrLn . unlines . map showEntry $ entries
       putStr "which one: "
       which <- getLine
-      let choose = entries !! (read which)
-      getTorrents $ snd . fst $ choose
-      
+      getTorrents . snd $ (entries !! (read which))
